ca35464136d94e7e874adb12d8250404b0856019
SmartQuail <smartquail.info@gmail.com>
2022-03-29 22:56:39 -0500

    adas
    dasd

--- qnode0_app_env_dev/qnode0_app/qnode0_app/settings.py
+++ qnode0_app_env_dev/qnode0_app/qnode0_app/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'wagtail.core',
     'wagtail.contrib.settings',
     'wagtail.contrib.routable_page',
-    'wagtail.contrib.postgres_search',
+   # 'wagtail.contrib.postgres_search',
     'wagtail.contrib.modeladmin',
     
     'taggit',
