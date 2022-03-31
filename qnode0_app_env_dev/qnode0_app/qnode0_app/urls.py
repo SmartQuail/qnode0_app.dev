b309949e60bbccfc7b7297b3f6e51d350ab829da
SmartQuail <smartquail.info@gmail.com>
2022-03-31 17:06:14 -0500

    urls

--- qnode0_app_env_dev/qnode0_app/qnode0_app/urls.py
+++ qnode0_app_env_dev/qnode0_app/qnode0_app/urls.py
@@ -1,21 +1,55 @@
-"""qnode0_app URL Configuration
+from django.urls import path,re_path,include
+from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
+#from noticias.sitemaps import PostSitemap
+
+#from baton.autodiscover import admin
 from django.contrib import admin
-from django.urls import path
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+#from qr_code import urls as qr_code_urls
+
+
+#sitemaps = {
+    #'posts': PostSitemap,
+#}
+
 
 urlpatterns = [
+    #path(_('payment/'), include('payment.urls', namespace='payment')),
+    #path(_('coupons/'), include('coupons.urls', namespace='coupons')),
     path('admin/', admin.site.urls),
-]
+    #path('sqaccounts/',include('sqaccounts.urls')),
+    #path(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
+    #path(_('ProFit/'), include('ProFit.urls')),
+    #path(_('baton/'),include('baton.urls')),
+    #path(_('cart/'), include('cart.urls', namespace='cart')),
+    #path('edificios/', include('HOMEDETAIL.urls', namespace='edificio')),
+    #path('cotizaciones/', include('HOMEDETAIL.urls', namespace='cotizacion')),
+    #path('qr-code/', include(qr_code_urls, namespace="qr_code")),
+    #path('services/', include('services.urls', namespace="services")),
+    #path(_('noticias/'), include('noticias.urls', namespace='noticias')),
+    #path(_('sitemap.xml'), sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    
+    #path(_('social-auth/'), include('social_django.urls', namespace='social')),
+    #path(_('images/'), include('images.urls', namespace='images')),
+    #path(_('Orders/'), include('orders.urls', namespace='orders')),
+    #path(_('cotizaciones_profit/'), include('cotizaciones.urls', namespace='cotizaciones')),
+    
+    
+    #path(_('rosetta/'),include('rosetta.urls')),
+    #path(_('Proyectos/'), include('Proyectos.urls', namespace='Proyectos')),
+
+    
+
+    re_path(r'^customers/', include(wagtailadmin_urls),name='wagtail'),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'', include(wagtail_urls)),
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
