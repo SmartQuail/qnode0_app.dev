21b6edf30353e2c856f375540aba969dd3e15e09
SmartQuail <smartquail.info@gmail.com>
2022-04-02 13:14:40 -0500

    Update settings.py

--- qnode0_app_env_dev/qnode0_app/qnode0_app/settings.py
+++ qnode0_app_env_dev/qnode0_app/qnode0_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get('DEBUG'))=="1"
-DEBUG = int(bool(os.environ.get('DEBUG',1)))    
+DEBUG = int(bool(os.environ.get('DEBUG',0)))    
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(None, os.environ.get('ALLOWED_HOSTS', '').split(','),
@@ -189,7 +189,7 @@ DB_IS_AVAIL = all([
 ])
 
 #POSTGRES_READY=str(os.environ.get('POSTGRES_READY'))=="1"
-POSTGRES_READY = int(bool(os.environ.get('POSTGRES_READ',1)))
+POSTGRES_READY = int(bool(os.environ.get('POSTGRES_READ',0)))
 if DB_IS_AVAIL and POSTGRES_READY:
     DATABASES = {
         'default': {
